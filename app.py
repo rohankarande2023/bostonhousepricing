9c378544cbe6add14caf0cd2236e1e6dae2fa0ae
Rohan Karande <rohankarande83@gmail.com>
2023-11-27 15:59:33 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods=["POST"])
-
 def predict_api():
     data=request.json['data']
     print(data)
@@ -25,6 +24,14 @@ def predict_api():
     output=regmodel.predict(std_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=["POST"])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=std_scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 
 if __name__ =='__main__':
